086cf4c49e41d93871ed1ce9fc82253fc6bc758e
jonascadona <jonascadona891@gmail.com>
2024-12-04 16:08:34 -0300

    alteração no server.js - 2

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 app.use(express.json());
+db.connect(err => {
+    if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+      console.log('Conectado ao banco de dados MySQL');
+    }
+  });
